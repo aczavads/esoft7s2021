dd6acae57c282cf91ba28ea7ecd49155b8ee6041
SantiagoPeres <santiagogperes@gmail.com>
2021-03-14 17:58:47 -0300

    Exemplo do zero de um componente estado e evento: (useState, useEffects)

--- spa/src/App.js
+++ spa/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import SomarComponent from './components/somar-components';
 import HelloComponent from './components/hello-component';
 import ContadorComponent from './components/contador-component';
+import ContadorBásico from './components/contador-basico';
 
 const somar = (v1,v2) => v1 + v2;
 
@@ -11,10 +12,11 @@ const resultado = somar(100,200);
 
   return(
     <div>
-      <div>{resultado}</div>  
+      <ContadorBásico></ContadorBásico>
+      {/* <div>{resultado}</div>  
       <SomarComponent v1 ={1000} v2 = {2020}></SomarComponent>
       <HelloComponent></HelloComponent>
-      <ContadorComponent valorMinimo = {10} valorMaximo = {15}></ContadorComponent>
+      <ContadorComponent valorMinimo = {10} valorMaximo = {15}></ContadorComponent> */}
     </div>
   );
 }
